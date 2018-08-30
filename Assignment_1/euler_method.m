214f44fece64cf3610b6e8ba55971c9957045989
praditya <adityapratap1.rajawat@gmail.com>
2018-08-30 17:54:08 +0530

    Initial commit : Euler method

---
+++ Assignment_1/euler_method.m
@@ -0,0 +1,30 @@
+function euler_method(delta_t, total_time)
+
+    % Given vortex flow equations
+    % r_dot = 0
+    % theta_dot = 5/r 
+    
+    % delta_t = 0.1;
+    % total_time = 5;
+    
+    t = 0:delta_t:total_time;
+    r = zeros(1,length(t));
+    theta = zeros(1,length(t));
+    
+    % Initial_conditions
+    r(1) = 1;
+    theta(1) = 0;
+    v_r = 0;
+    
+    for i=1:length(t)-1
+        %forward_euler
+        r(i+1) = r(i) + delta_t*v_r;
+        theta(i+1) = theta(i) + delta_t*5/(r(i)^2);
+    end
+    
+    polar(theta,r)
+%     legend('theta','radius');
+    title('Polar Plot');
+    xlabel('Time(s)');
+    
+end
